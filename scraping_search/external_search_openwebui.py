b1be1953fd00976d62e1b6281fc653357e9fe14d
kleyton67 <kleyton.sartori.leite@gmail.com>
2025-05-24 02:33:32 -0300

    feat: first commit

---
+++ scraping_search/external_search_openwebui.py
@@ -0,0 +1,39 @@
+import uvicorn
+from fastapi import FastAPI, Header, Body, HTTPException
+from pydantic import BaseModel
+from scraping_ddkg import ddkg_search
+from typing import List
+from loguru import logger
+# from duckduckgo_search import DDGS
+
+EXPECTED_BEARER_TOKEN = "your_secret_token_here"
+
+app = FastAPI()
+
+
+class SearchRequest(BaseModel):
+    query: str
+    count: int
+
+
+class SearchResult(BaseModel):
+    link: str
+    title: str | None
+    snippet: str | None
+
+
+@app.post("/search")
+async def external_search(
+    search_request: SearchRequest = Body(...),
+    authorization: str | None = Header(None),
+):
+    
+    results_list = ddkg_search(f'https://duckduckgo.com/?t=h_&q={search_request.query}&ia=web')
+
+    logger.info("Results from Duckduckgo:")
+    logger.info(results_list)
+    return results_list
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=80)
---
+++ scraping_search/scraping_ddkg.py
@@ -0,0 +1,71 @@
+
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.firefox.service import Service
+from bs4 import BeautifulSoup
+from pydantic import BaseModel
+from selenium.webdriver.remote.webelement import WebElement
+from typing import List
+from time import sleep
+
+class SearchResult(BaseModel):
+    title: str
+    link: str
+    description: str
+
+def ddkg_search(url):
+    # Set up options
+    options = Options()
+    options.add_argument("-headless")  # For headless testing
+    options.assume_request_during_headless = True
+    # options.binary_location = "/opt/waterfox/waterfox"
+    options.binary_location = "librewolf"
+    # driver_path = "/opt/geckodriver/geckodriver"
+    driver_path = "/usr/src/app/geckodriver"
+
+    # firefox_profile.set_preference(
+    #     'intl.accept_languages', 'en-US'
+    # )
+    options.set_preference(
+        'geo.enabled', True
+    )
+    options.set_preference('geo.provider.network.url',
+    'data:application/json,{"location": {"lat": 51.47, "lng": 0.0}, "accuracy": 100.0}')
+    # options.profile = firefox_profile
+
+    service = Service(executable_path=driver_path)
+
+    driver = webdriver.Firefox(service=service, options=options)
+
+    driver.get(url)
+
+    wait = WebDriverWait(driver, 10)
+    element = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "react-results--main")))
+
+    list_items : List[WebElement] =  element.find_elements(By.CSS_SELECTOR, 'li[data-layout="organic"]')
+
+    results_list = []
+    for item in list_items:
+        #item.get_attribute("innerHTML") to see all html of this component
+        sleep(1)
+        results_list.append(
+            
+            SearchResult(
+                title=item.find_element(By.CSS_SELECTOR, 'h2 a').text,
+                link=item.find_elements(By.CSS_SELECTOR, 'h2 a')[0].get_attribute("href"),
+                description=item.find_elements(By.CSS_SELECTOR, 'div [data-result="snippet"]')[0].text
+            )
+        )
+        
+        
+    
+    return results_list
+if __name__ == "__main__":
+    # Example usage
+    l_results = ddkg_search("https://duckduckgo.com/?q=asdkf")
+
+    import pdb
+    pdb.set_trace()
