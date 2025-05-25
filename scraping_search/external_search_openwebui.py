e960ef606a5b24d344b1d3c179465e2341cfc3dd
kleyton67 <kleyton.sartori.leite@gmail.com>
2025-05-25 17:17:58 -0300

    fix: added middleware to measure processtime

--- scraping_search/external_search_openwebui.py
+++ scraping_search/external_search_openwebui.py
@@ -1,14 +1,37 @@
 import uvicorn
-from fastapi import FastAPI, Header, Body, HTTPException
+from fastapi import FastAPI, Header, Body, Request, Response
 from pydantic import BaseModel
 from scraping_ddkg import ddkg_search
 from typing import List
 from loguru import logger
+from time import process_time
 # from duckduckgo_search import DDGS
 
 EXPECTED_BEARER_TOKEN = "your_secret_token_here"
 
 app = FastAPI()
+
+
+# Middleware to log requests and responses with processing time
+class LoggingMiddleware:
+    def __init__(self, app):
+        self.app = app
+
+    async def __call__(self, request: Request, call_next: Response):
+        start_time = process_time()  # Start timer to measure processing time
+
+        response = await call_next(request)  # Call the next middleware or route handler
+
+        logger.info(f"Request Method: {request.method}")
+        logger.info(f"Request Path: {request.url.path}")
+        logger.info(f"Response Status Code: {response.status_code}")
+        process_time_diff = process_time() - start_time  # Calculate processing time
+        logger.info(f"Processing Time: {process_time_diff:.4f} seconds")
+        return response
+
+
+# Mount the middleware to the FastAPI app
+app.add_middleware(LoggingMiddleware)
 
 
 class SearchRequest(BaseModel):
@@ -27,8 +50,11 @@ async def external_search(
     search_request: SearchRequest = Body(...),
     authorization: str | None = Header(None),
 ):
-    
-    results_list = ddkg_search(f'https://duckduckgo.com/?t=h_&q={search_request.query}&ia=web', search_request.count)
+    logger.info(f"Searching for {search_request.query}")
+    results_list = ddkg_search(
+        f"https://duckduckgo.com/?t=h_&q={search_request.query}&ia=web",
+        search_request.count,
+    )
 
     logger.info("Results from Duckduckgo:")
     logger.info(results_list)
